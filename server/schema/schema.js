9d99acc84e7d07f1fa63f6988ffafa52bdcd8402
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2022-12-28 08:06:05 +0100

    add mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,16 @@
-const { projects, clients } = require('../sampleData.js');
+
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 const {
     GraphQLObjectType,
     GraphQLID,
     GraphQLString,
     GraphQLSchema,
     GraphQLList,
-} = require('graphql')
+    GraphQLNonNull,
+
+} = require('graphql');
+const { Query } = require('mongoose');
 
 // Client Type
 const ClientType = new GraphQLObjectType({
@@ -31,11 +36,13 @@ const ProjectType = new GraphQLObjectType({
             type: ClientType,
             resolve(parent, args)
             {
-                return clients.find((client) => client.id === parent.clientId);
+                return clients.findById(parent.clientId);
             }
         }
     }),
 });
+
+// Query
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -44,7 +51,7 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(ProjectType),
             resolve(parent, args)
             {
-                return projects;
+               return Project.find();
             }
         },
         project: {
@@ -52,7 +59,7 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } }, // args is an object that contains the arguments that we pass to the query
             resolve(parent, args)
             {
-                return projects.find((project) => project.id === args.id);
+                return Project.findById(args.id);
             },
         },
     
@@ -61,7 +68,7 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(ClientType),
             resolve(parent, args)
             {
-                return clients;
+                return Client.find();
             }
         },
         client: {
@@ -69,10 +76,36 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } }, // args is an object that contains the arguments that we pass to the query
             resolve(parent, args)
             {
-                return clients.find((client) => client.id === args.id);
+                return Client.findById(args.id);
             },
         },
     },
+});
+// MUtation
+const mutation = new GraphQLObjectType ({
+    name: 'Mutation',
+    fields: {
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) },
+                email: { type: GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLNonNull(GraphQLString) },  
+            },
+            resolve(parent, args)
+            {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone,
+
+                });
+                return client.save();
+            }
+        } 
+    }
+    
+
 });
 
 module.exports = new GraphQLSchema({
