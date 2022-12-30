2d1347abf4c1c021c618c7c5b6c21ce0c5de6104
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2022-12-30 10:30:52 +0100

    add folder querise

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,23 +1,6 @@
 import { FaTrash } from "react-icons/fa";
-import { useMutation } from "@apollo/client";
-import { DELETE_CLIENT } from "../mutations/clientMutations";
-import { GET_CLIENTS } from "../queries/clientQueries";
-import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow({ client }) {
-  const [deleteClient] = useMutation(DELETE_CLIENT, {
-    variables: { id: client.id },
-    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
-    // update(cache, { data: { deleteClient } }) {
-    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
-    //   cache.writeQuery({
-    //     query: GET_CLIENTS,
-    //     data: {
-    //       clients: clients.filter((client) => client.id !== deleteClient.id),
-    //     },
-    //   });
-    // },
-  });
 
   return (
     <tr>
@@ -25,7 +8,7 @@ export default function ClientRow({ client }) {
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+        <button className="btn btn-danger btn-sm">
           <FaTrash />
         </button>
       </td>
