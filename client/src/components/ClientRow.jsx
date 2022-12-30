9768b3e47475c610dd2d0c62973a74f27b1e11af
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2022-12-30 10:36:55 +0100

    add folder mutation for delete and update client

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,6 +1,23 @@
-import { FaTrash } from "react-icons/fa";
+import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations';
+import { GET_CLIENTS } from '../queries/clientQueries';
+// import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
+  });
 
   return (
     <tr>
@@ -8,7 +25,7 @@ export default function ClientRow({ client }) {
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className='btn btn-danger btn-sm' onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
